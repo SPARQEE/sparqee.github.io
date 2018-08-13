b0327861b40850bf8d8b883f763670680215e89f
Yas Oloumi <yasoloumi@Yass-MBP.hitronhub.home>
2018-08-12 21:17:08 -0700

    initial set of files

--- Desktop/Bootcamp trial/bootcamp-yasiol-master/src/main/java/Shape.java
+++
@@ -1,7 +0,0 @@
-public interface Shape {
-	public double parameter();
-	public double area();
-	//Usually don't write any code in interface; what you can do with shape: find area
-	//and parameter, no code on how to do it, just way to define method,
-	//not how it is executed
-}
